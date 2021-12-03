7d69938679e05f8e359423048faec6dcf9814db3
Matt Hinz <matthinz@gmail.com>
2021-12-03 15:33:25 -0800

    Update 03-01 to use bit twiddling / shifting
    Use bit twiddling / shifting rather than reading binary numbers as strings

--- day03/puzzle01.go
+++ day03/puzzle01.go
@@ -3,55 +3,74 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
 
-	var bitLength int
-	var numberCount int
-	// tracks the number of '1' bits at each corresponding position
-	var oneCounts []int
+	numbers := readBinaryNumberList(os.Stdin)
 
-	for scanner.Scan() {
-		value := scanner.Text()
-		if len(value) == 0 {
-			continue
-		}
-
-		if bitLength == 0 {
-			bitLength = len(value)
-			oneCounts = make([]int, bitLength)
-		} else if len(value) != bitLength {
-			panic("invalid bit length")
-		}
-
-		numberCount++
-
-		for index, c := range value {
-			if c == '1' {
-				oneCounts[index]++
-			}
-		}
-	}
-
-	// gamma is a `bitLength`-length binary number whe
-	var gamma uint
-	var epsilon uint
-
-	for index, count := range oneCounts {
-		if count >= numberCount/2 {
-			// this bit should be 1
-			gamma = gamma | 1<<(bitLength-(index)-1)
-		} else {
-			epsilon = epsilon | 1<<(bitLength-(index)-1)
-		}
-	}
+	gamma, epsilon := CalculateGammaAndEpsilon(numbers)
 
 	fmt.Printf("gamma: %s (%d)\n", strconv.FormatInt(int64(gamma), 2), gamma)
 	fmt.Printf("epsilon: %s (%d)\n", strconv.FormatInt(int64(epsilon), 2), epsilon)
 	fmt.Printf("product: %d\n", gamma*epsilon)
+}
 
+func CalculateGammaAndEpsilon(numbers []int) (int, int) {
+	bitLength := 16
+	setBits := make([]int, bitLength)
+
+	// Create a map of bit index to number of elements that have the corresponding bit set
+	for _, value := range numbers {
+		for i := 0; i < bitLength; i++ {
+			mask := 1 << i
+			bitIsSet := value&mask != 0
+			if bitIsSet {
+				setBits[i]++
+			}
+		}
+	}
+
+	fmt.Println(setBits)
+
+	var gamma, epsilon int
+
+	for i := 0; i < bitLength; i++ {
+		countWithBitSet := setBits[i]
+
+		if countWithBitSet == 0 {
+			continue
+		}
+
+		if countWithBitSet >= len(numbers)/2 {
+			// when >= 50% have bit set, set corresponding bit in gamma
+			gamma = gamma | 1<<i
+		} else {
+			// when < 50% have bit set, set corresponding bit in epsilon
+			epsilon = epsilon | 1<<i
+		}
+	}
+
+	return gamma, epsilon
+}
+
+func readBinaryNumberList(r io.Reader) []int {
+	scanner := bufio.NewScanner(os.Stdin)
+	var result []int
+
+	for scanner.Scan() {
+		token := scanner.Text()
+		if len(token) == 0 {
+			continue
+		}
+		value, err := strconv.ParseInt(token, 2, 64)
+		if err != nil {
+			panic(err)
+		}
+		result = append(result, int(value))
+	}
+	return result
 }
