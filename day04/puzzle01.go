a6682f533908f19462fc67c379867beebc088848
Matt Hinz <matthinz@gmail.com>
2021-12-04 13:46:06 -0800

    Day 4 (squid bingo)

---
+++ day04/puzzle01.go
@@ -0,0 +1,239 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type square struct {
+	value  int
+	marked bool
+}
+
+type Board struct {
+	index   int
+	squares [][]square
+}
+
+type game struct {
+	numbers []int
+	boards  []Board
+}
+
+type solvedBoard struct {
+	b          Board
+	finalScore int
+}
+
+func main() {
+	game := NewGame(os.Stdin)
+	solvedBoards := game.Run()
+
+	for _, sb := range solvedBoards {
+		fmt.Printf("Board: %d/%d\nScore:%d\n", sb.b.index, len(solvedBoards), sb.finalScore)
+	}
+}
+
+func (g *game) Run() []solvedBoard {
+
+	var result []solvedBoard
+
+	unsolvedBoards := g.boards
+
+	for _, number := range g.numbers {
+		for i := 0; i < len(unsolvedBoards); i++ {
+			b := &unsolvedBoards[i]
+			b.markNumber(number)
+
+			if b.isSolved() {
+
+				finalScore := b.sumOfUnmarkedSquares() * number
+
+				fmt.Printf("%d: %d * %d = %d\n", b.index, b.sumOfUnmarkedSquares(), number, finalScore)
+
+				result = append(result, solvedBoard{
+					b:          *b,
+					finalScore: finalScore,
+				})
+
+				unsolvedBoards = removeBoardAt(unsolvedBoards, i)
+				i--
+			}
+		}
+	}
+
+	return result
+}
+
+func (b *Board) String() string {
+	result := strings.Builder{}
+
+	for _, row := range b.squares {
+		for _, square := range row {
+			if square.marked {
+				result.WriteString("(")
+			} else {
+				result.WriteString(" ")
+			}
+			if square.value < 10 {
+				result.WriteString(" ")
+			}
+			result.WriteString(strconv.FormatInt(int64(square.value), 10))
+			if square.marked {
+				result.WriteString(")")
+			} else {
+				result.WriteString(" ")
+			}
+		}
+		result.WriteString("\n")
+	}
+
+	return result.String()
+
+}
+
+func (b *Board) markNumber(number int) {
+	for y := range b.squares {
+		for x := range b.squares[y] {
+			s := &b.squares[y][x]
+			if s.value == number {
+				s.marked = true
+			}
+		}
+	}
+}
+
+func (b *Board) isSolved() bool {
+	if len(b.squares) == 0 {
+		return false
+	}
+
+	height := len(b.squares)
+	width := len(b.squares[0])
+
+	rowStates := make([]bool, height)
+	colStates := make([]bool, width)
+
+	// initialize state maps to true
+
+	for y := 0; y < height; y++ {
+		rowStates[y] = true
+	}
+
+	for x := 0; x < width; x++ {
+		colStates[x] = true
+	}
+
+	for y, row := range b.squares {
+		for x, square := range row {
+			rowStates[y] = rowStates[y] && square.marked
+			colStates[x] = colStates[x] && square.marked
+		}
+	}
+
+	var anyRows bool
+	var anyCols bool
+
+	for _, ok := range rowStates {
+		if ok {
+			anyRows = true
+		}
+	}
+
+	for _, ok := range colStates {
+		if ok {
+			anyCols = true
+		}
+	}
+
+	return anyRows || anyCols
+}
+
+func (b *Board) sumOfUnmarkedSquares() int {
+	var result int
+
+	for _, row := range b.squares {
+		for _, square := range row {
+			if !square.marked {
+				result += square.value
+			}
+
+		}
+	}
+
+	return result
+}
+
+func NewGame(r io.Reader) game {
+
+	scanner := bufio.NewScanner(r)
+
+	var g *game
+	var b *Board
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if len(line) == 0 {
+			b = nil
+			continue
+		}
+
+		if g == nil {
+			// first line of input = numbers
+			g = &game{
+				numbers: parseNumbers(line, ","),
+			}
+			continue
+		}
+
+		// subsequent lines of numbers = board
+		if b == nil {
+			g.boards = append(g.boards, Board{
+				index: len(g.boards) + 1,
+			})
+			b = &g.boards[len(g.boards)-1]
+		}
+
+		b.squares = append(b.squares, parseRow(line))
+	}
+
+	return *g
+}
+
+func parseNumbers(input string, sep string) []int {
+	var result []int
+
+	for _, token := range strings.Split(input, sep) {
+		value, err := strconv.ParseInt(token, 10, 32)
+		if err == nil {
+			result = append(result, int(value))
+		}
+	}
+
+	return result
+}
+
+func parseRow(input string) []square {
+	var result []square
+	for _, value := range parseNumbers(input, " ") {
+		result = append(result, square{
+			value: value,
+		})
+	}
+	return result
+}
+
+func removeBoardAt(boards []Board, index int) []Board {
+	var result []Board
+	for i := range boards {
+		if i != index {
+			result = append(result, boards[i])
+		}
+	}
+	return result
+}
---
+++ day04/puzzle01_test.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestNewGame(t *testing.T) {
+	input := `
+1,2,3,4
+
+1 2
+3 4
+
+5 6
+7 8
+`
+
+	game := NewGame(strings.NewReader(input))
+
+	assertIntSlicesEqual(t, []int{1, 2, 3, 4}, game.numbers)
+
+	assertIntSlicesEqual(t, []int{1, 2}, getSquareValues(game.boards[0].squares[0]))
+
+}
+
+func TestRunGame(t *testing.T) {
+	input := `
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+8  2 23  4 24
+21  9 14 16  7
+6 10  3 18  5
+1 12 20 15 19
+
+3 15  0  2 22
+9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+2  0 12  3  7
+	`
+
+	game := NewGame(strings.NewReader(input))
+
+	assertIntSlicesEqual(
+		t,
+		game.numbers,
+		[]int{7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1},
+	)
+
+	assertIntSlicesEqual(
+		t,
+		getSquareValues(game.boards[0].squares[0]),
+		[]int{22, 13, 17, 11, 0},
+	)
+
+	solution := game.Run()
+
+	firstWinner := solution[0]
+	if firstWinner.b.index != 3 {
+		fmt.Println(firstWinner.b.String())
+		t.Error("The wrong board won first")
+	}
+
+	lastWinner := solution[len(solution)-1]
+	if lastWinner.b.index != 2 {
+		fmt.Println(lastWinner.b.String())
+		t.Error("the wrong board was last to win")
+	}
+
+	if lastWinner.finalScore != 1924 {
+		fmt.Println(lastWinner.b.String())
+		t.Error("the last board had the wrong score")
+	}
+
+}
+
+func assertIntSlicesEqual(t *testing.T, x []int, y []int) {
+	if len(x) != len(y) {
+		t.Error(fmt.Sprintf("Lengths don't match: %d vs %d", len(x), len(y)))
+		return
+	}
+
+	for i := range x {
+		if x[i] != y[i] {
+			t.Error(fmt.Sprintf("Values don't match at position %d: %d != %d", i, x[i], y[i]))
+		}
+	}
+}
+
+func getSquareValues(squares []square) []int {
+	var result []int
+	for i := range squares {
+		result = append(result, squares[i].value)
+	}
+	return result
+}
