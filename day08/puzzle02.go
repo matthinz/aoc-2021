818d9c557b98fc7d94c64f95d88ee3babc567100
Matt Hinz <matthinz@gmail.com>
2021-12-08 15:34:54 -0800

    Day 8 - digits digits digits

--- day08/puzzle02.go
+++ day08/puzzle02.go
@@ -4,43 +4,82 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"sort"
 	"strings"
 )
 
 type input struct {
-	signalValues []signalValueInput
+	signalValues []string
 	outputValues []string
 }
 
-type signalValueInput struct {
-	unknownValues []string
-	knownValues   []string
+var SignalValuesForNumbers = [10]string{
+	"abcefg",
+	"cf",
+	"acdeg",
+	"acdfg",
+	"bcdf",
+	"abdfg",
+	"abdefg",
+	"acf",
+	"abcdefg",
+	"abcdfg",
 }
 
 func main() {
 	inputs := parseInput(os.Stdin)
-	var ct int
+	ch := make(chan int)
+
 	for _, i := range inputs {
-		for _, val := range i.outputValues {
-			switch len(val) {
-			case 2:
-				ct++
-				break
-			case 3:
-				ct++
-				break
-			case 4:
-				ct++
-				break
-			case 7:
-				ct++
-				break
-			}
+		go func(i input) {
+			key := interpretSignalValues(i.signalValues)
+			digits := getDigits(i, key)
+			ch <- makeIntFromDigits(digits)
+		}(i)
+	}
+
+	var received int
+	var sum int
+
+	for num := range ch {
+		received++
+
+		sum += num
+		fmt.Printf("%d (total %d) - (%d / %d)\n", num, sum, received, len(inputs))
+
+		if received == len(inputs) {
+			close(ch)
+			break
 		}
 	}
-	fmt.Println(ct)
+
+}
+
+func makeIntFromDigits(digits []int) int {
+	var result int
+	for i, digit := range digits {
+		result += (digit * int(math.Pow10(len(digits)-(i+1))))
+	}
+	return result
+}
+
+func getDigits(i input, key []int) []int {
+
+	result := make([]int, len(i.outputValues))
+
+	for outputIndex, v := range i.outputValues {
+		inputIndex := stringSliceIndexOf(i.signalValues, v)
+		if inputIndex < 0 {
+			panic(fmt.Sprintf("output %s not found in signal values %v", v, i.signalValues))
+		}
+		digit := key[inputIndex]
+		result[outputIndex] = digit
+	}
+
+	return result
+
 }
 
 func parseInput(r io.Reader) []input {
@@ -55,6 +94,10 @@ func parseInput(r io.Reader) []input {
 			continue
 		}
 
+		result = append(result, input{
+			signalValues: parseSegments(parts[0]),
+			outputValues: parseSegments(parts[1]),
+		})
 	}
 
 	return result
@@ -62,208 +105,162 @@ func parseInput(r io.Reader) []input {
 
 func parseSegments(input string) []string {
 	input = strings.Trim(input, " \t\r\n")
-	return strings.Split(input, " ")
+	segments := strings.Split(input, " ")
+
+	result := make([]string, len(segments))
+	for i, segment := range segments {
+		runes := []rune(segment)
+		sort.Slice(runes, func(i, j int) bool {
+			return runes[i] < runes[j]
+		})
+		result[i] = string(runes)
+	}
+	return result
 }
 
 func interpretSignalValues(inputs []string) []int {
 
-	solvedNumbers := make(map[string]int)
+	to := "abcdefg"
+	var solution string
+	var ct int
 
-	segmentMappings := make(map[rune]rune)
-
-	unsolvedInputs := inputs
-
-	tickIndex := 0
-
-	for len(unsolvedInputs) > 0 {
-		tickIndex++
-
-		if tickIndex > 3 {
+	for from := range generateStrings(len(to)) {
+		ct++
+		ok := areMappingsValid(from, to, inputs)
+		if ok {
+			solution = from
 			break
 		}
-
-		unsolvedInputs = tick(unsolvedInputs, &solvedNumbers, &segmentMappings)
-
-		fmt.Printf("*** TICK %d SOLVED: %v ***\n", tickIndex, solvedNumbers)
-		fmt.Printf("*** TICK %d UNSOLVED: %v ***\n", tickIndex, unsolvedInputs)
-
 	}
 
-	fmt.Println(solvedNumbers)
-
-	if len(solvedNumbers) != len(inputs) {
-		panic(fmt.Sprintf("Expected %d solved inputs, but got %d", len(inputs), len(solvedNumbers)))
+	if len(solution) == 0 {
+		return []int{}
 	}
 
-	result := make([]int, 0, len(inputs))
+	// result is a slice of ints where each number corresponds to the input
+	result := make([]int, len(inputs))
 
-	for i := 0; i < len(inputs); i++ {
-		result = append(result, solvedNumbers[inputs[i]])
+	for i, input := range inputs {
+		transformed := applyTransform(input, solution, to)
+		num := stringSliceIndexOf(SignalValuesForNumbers[:], transformed)
+		if num < 0 {
+			panic(fmt.Sprintf("solution %s does not solve %s", solution, input))
+		}
+		result[i] = num
 	}
 
 	return result
 }
 
-func tick(inputSignalValues []string, solvedNumbers *map[string]int, segmentMappings *map[rune]rune) []string {
-	segmentsForNumbers := []string{
-		"abcefg",
-		"cf",
-		"acdeg",
-		"acdfg",
-		"bcdf",
-		"abdfg",
-		"abdefg",
-		"acf",
-		"abcdefg",
-		"abcdfg",
+func buildStrings(targetLength int, alphabet string, ch chan string, input string) {
+	if len(input) == targetLength {
+		ch <- input
+		return
 	}
-
-	numbersForSegments := make(map[string]int)
-	for number, segments := range segmentsForNumbers {
-		numbersForSegments[segments] = number
+	for _, c := range alphabet {
+		buildStrings(targetLength, alphabet, ch, input+string(c))
 	}
-
-	// step 1: try and resolve any segments we can
-
-	for _, inputSignalValue := range inputSignalValues {
-		// inputSignalValue is e.g. "adf"
-
-		// build a list of candidates that are the same length
-		candidates := make(map[int]bool)
-		var candidateNumber int
-		for num := 0; num <= 9; num++ {
-
-			isSolved := false
-			for _, n := range *solvedNumbers {
-				if n == num {
-					isSolved = true
-					break
-				}
-			}
-
-			if isSolved {
-				continue
-			}
-
-			if len(segmentsForNumbers[num]) == len(inputSignalValue) {
-				candidates[num] = true
-				candidateNumber = num
-			}
-		}
-
-		if len(candidates) != 1 {
-			continue
-		}
-
-		fmt.Printf("candidate for %s: %v\n", inputSignalValue, candidateNumber)
-
-		candidateNumberSegments := segmentsForNumbers[candidateNumber]
-
-		candidateMappings := make(map[rune][]rune)
-
-		for _, inputSegment := range inputSignalValue {
-			for _, knownSegment := range candidateNumberSegments {
-				// if the # of input values w/ the input segment != # of known values w/ known segment, that's bad
-				// also, if the individual input values w/ in the input segment don't have the same lengths as the known values, that's bad
-
-				inputValuesWithSegment := findValuesWithSegment(inputSignalValues, inputSegment)
-				sort.Slice(inputValuesWithSegment, func(i, j int) bool {
-					return len(inputValuesWithSegment[i]) < len(inputValuesWithSegment[j])
-				})
-
-				knownValuesWithSegment := findValuesWithSegment(segmentsForNumbers, knownSegment)
-				sort.Slice(knownValuesWithSegment, func(i, j int) bool {
-					return len(knownValuesWithSegment[i]) < len(knownValuesWithSegment[j])
-				})
-
-				if len(inputValuesWithSegment) != len(knownValuesWithSegment) {
-					continue
-				}
-
-				fmt.Printf("Input values with segment %s: %v\n", string(inputSegment), inputValuesWithSegment)
-				fmt.Printf("Known values with segment %s: %v\n", string(knownSegment), knownValuesWithSegment)
-
-				allLengthsMatch := true
-
-				for i := range inputValuesWithSegment {
-					allLengthsMatch = allLengthsMatch && len(inputValuesWithSegment[i]) == len(knownValuesWithSegment[i])
-				}
-
-				if !allLengthsMatch {
-					continue
-				}
-
-				fmt.Printf("Candidate mapping: %s -> %s\n", string(inputSegment), string(knownSegment))
-				candidateMappings[inputSegment] = append(candidateMappings[inputSegment], knownSegment)
-			}
-		}
-
-		for inputSegment, candidateSegments := range candidateMappings {
-			if len(candidateSegments) == 1 {
-				fmt.Printf("segment %s == %s\n", string(inputSegment), string(candidateSegments[0]))
-			}
-			(*segmentMappings)[inputSegment] = candidateSegments[0]
-		}
-	}
-
-	fmt.Printf("known segment mappings: %v\n", *segmentMappings)
-
-	// step 2: given our known segment mappings, see if we can solve anything
-	unsolvedInputs := make([]string, 0, len(inputSignalValues))
-	potentialSolves := make(map[int][]string)
-
-	for _, inputSignalValue := range inputSignalValues {
-
-		// if every segment in this value can be mapped, do it
-		ok := true
-		segments := make([]rune, 0, len(inputSignalValue))
-
-		for _, inputSegment := range inputSignalValue {
-			knownSegment, found := (*segmentMappings)[inputSegment]
-			fmt.Printf("%s = %s, %v\n", string(inputSegment), string(knownSegment), found)
-			if !found {
-				ok = false
-				break
-			}
-			segments = append(segments, knownSegment)
-		}
-
-		if !ok {
-			// try this input on the next tick
-			fmt.Printf("Unsolvable input: %s\n", inputSignalValue)
-			unsolvedInputs = append(unsolvedInputs, inputSignalValue)
-			continue
-		}
-
-		knownValue := string(segments)
-		number := numbersForSegments[knownValue]
-
-		potentialSolves[number] = append(potentialSolves[number], inputSignalValue)
-
-		fmt.Printf("____ %s maps to %s, which == %d ____\n", inputSignalValue, knownValue, number)
-	}
-
-	// step 3 for times where we were able to come up with 1 solution, keep it
-
-	for number, inputSignalValues := range potentialSolves {
-
-		fmt.Printf("%d solves %v\n", number, inputSignalValues)
-
-		if len(inputSignalValues) > 1 {
-			for _, i := range inputSignalValues {
-				unsolvedInputs = append(unsolvedInputs, i)
-			}
-			continue
-		}
-
-		(*solvedNumbers)[inputSignalValues[0]] = number
-	}
-
-	return unsolvedInputs
-
 }
 
-func findValuesWithLength(values []string, length int) []string {
+func generateStrings(targetLength int) chan string {
+	ch := make(chan string)
+
+	go func() {
+		buildStrings(targetLength, "abcdefg", ch, "")
+		close(ch)
+	}()
+
+	return ch
+}
+
+// given an input string, converts each occurrence of a character in
+// <from> to the corresponding character in <to>
+func applyTransform(input string, from string, to string) string {
+
+	result := make([]rune, len(input))
+
+	for i, r := range input {
+		fromIndex := strings.IndexRune(from, r)
+		if fromIndex >= 0 {
+			result[i] = rune(to[fromIndex])
+		} else {
+			result[i] = r
+		}
+	}
+
+	// sort the result a->z
+	sort.Slice(result, func(i, j int) bool { return result[i] < result[j] })
+
+	return string(result)
+}
+
+func areMappingsValid(from string, to string, inputs []string) bool {
+
+	// in each input, change each character in <from> to the corresponding
+	// character in <to>
+
+	transformedInputs := make([]string, len(inputs))
+	for i, input := range inputs {
+		transformedInputs[i] = applyTransform(input, from, to)
+	}
+
+	// having been transformed, verify that each new input is present
+	// in our known good set of numbers
+
+	for _, transformedInputSignalValue := range transformedInputs {
+		found := false
+		for _, knownGoodSignalValue := range SignalValuesForNumbers {
+			if transformedInputSignalValue == knownGoodSignalValue {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+
+	return true
+}
+
+func stringSliceIndexOf(slice []string, value string) int {
+	for i := range slice {
+		if slice[i] == value {
+			return i
+		}
+	}
+	return -1
+}
+
+func stringSliceContains(slice []string, value string) bool {
+	for i := range slice {
+		if slice[i] == value {
+			return true
+		}
+	}
+	return false
+}
+
+func replaceRunesAndSort(input string, from []rune, to []rune) string {
+	if len(from) != len(to) {
+		panic(fmt.Sprintf("wrong length (%d vs %d)", len(from), len(to)))
+	}
+	result := make([]rune, len(input))
+	for i, c := range input {
+		for j := range from {
+			if c == from[j] {
+				result[i] = to[j]
+			} else {
+				result[i] = c
+			}
+		}
+	}
+	sort.Slice(result, func(i, j int) bool { return result[i] < result[j] })
+
+	return string(result)
+}
+
+func findStringsWithLength(values []string, length int) []string {
 	var result []string
 	for _, value := range values {
 		if len(value) == length {
@@ -284,16 +281,6 @@ func findValuesWithSegment(signalValues []string, segment rune) []string {
 	result := make([]string, 0, len(resultMap))
 	for value := range resultMap {
 		result = append(result, value)
-	}
-	return result
-}
-
-func removeSolvedInputs(input []signalValueInput) []signalValueInput {
-	var result []signalValueInput
-	for _, i := range input {
-		if len(i.unknownValues) == 0 {
-			result = append(result, i)
-		}
 	}
 	return result
 }
