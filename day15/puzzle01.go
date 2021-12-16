772618e281c8870cbb9d384a00680c53010d480e
Matt Hinz <matthinz@gmail.com>
2021-12-15 21:27:37 -0800

    Day 15 (risky paths) part 1

---
+++ day15/puzzle01.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type point struct {
+	x         int
+	y         int
+	risk      int
+	totalRisk int
+}
+
+const unknownRisk = -1
+
+func main() {
+
+	grid := parseInput(os.Stdin)
+	lowestTotalRisk := solveDijkstra(grid)
+	fmt.Println(lowestTotalRisk)
+
+	grid = inflateGrid(grid, 5)
+	lowestTotalRisk = solveDijkstra(grid)
+	fmt.Println(lowestTotalRisk)
+}
+
+func inflateGrid(grid *[][]int, inflationFactor int) *[][]int {
+	height := len(*grid)
+	width := len((*grid)[0])
+	inflatedGrid := make([][]int, height*inflationFactor)
+
+	for y := 0; y < height; y++ {
+		for i := 0; i < inflationFactor; i++ {
+			destY := (height * i) + y
+			inflatedGrid[destY] = make([]int, width*inflationFactor)
+
+			for x := 0; x < width; x++ {
+				for j := 0; j < inflationFactor; j++ {
+
+					destX := (width * j) + x
+
+					value := ((*grid)[y][x] + i + j)
+					if value > 9 {
+						value = value % 9
+					}
+
+					inflatedGrid[destY][destX] = value
+				}
+			}
+		}
+	}
+
+	return &inflatedGrid
+}
+
+func solveDijkstra(grid *[][]int) int {
+
+	height := len(*grid)
+	width := len((*grid)[0])
+
+	// toVisit is a list of points, sorted by totalRisk -- from
+	// highest risk to lowest risk
+	toVisitLength := width * height
+	toVisit := make([]point, 0, toVisitLength)
+
+	// going in reverse here so that toVisit ends up with
+	// lowest totalRisk at the end
+	for y := height - 1; y >= 0; y-- {
+		for x := width - 1; x >= 0; x-- {
+			totalRisk := unknownRisk
+
+			if x == 0 && y == 0 {
+				// starting location = no risk
+				totalRisk = 0
+			}
+
+			p := point{
+				x:         x,
+				y:         y,
+				risk:      (*grid)[y][x],
+				totalRisk: totalRisk,
+			}
+
+			toVisit = append(toVisit, p)
+		}
+	}
+
+	visited := make([]point, 0, width*height)
+
+	for len(toVisit) > 0 {
+
+		if rand.Float64() < 0.001 {
+			left := len(toVisit)
+			if left > 10000 {
+				fmt.Fprintf(os.Stderr, "%d to visit\n", left)
+			}
+		}
+
+		// Get the last point in toVisit, which will be the *least* risky
+		point, nextToVisit := popPoint(toVisit)
+		toVisit = nextToVisit
+
+		visited = append(visited, point)
+
+		needToSort := false
+
+		for i := range toVisit {
+
+			isNorthNeighbor := (toVisit[i].x == point.x && toVisit[i].y == point.y-1)
+			isSouthNeighbor := (toVisit[i].x == point.x && toVisit[i].y == point.y+1)
+			isEastNeighbor := (toVisit[i].x == point.x+1 && toVisit[i].y == point.y)
+			isWestNeighbor := (toVisit[i].x == point.x-1 && toVisit[i].y == point.y)
+
+			isNeighbor := isNorthNeighbor || isSouthNeighbor || isEastNeighbor || isWestNeighbor
+
+			if !isNeighbor {
+				continue
+			}
+
+			// we've found an unvisited neighbor compute its total risk, then update
+			// its position in <toVisit>
+			neighbor := &toVisit[i]
+			newRisk := point.totalRisk + neighbor.risk
+			if neighbor.totalRisk == unknownRisk || neighbor.totalRisk > newRisk {
+				neighbor.totalRisk = newRisk
+				needToSort = true
+			}
+		}
+
+		if !needToSort {
+			continue
+		}
+
+		// // keep toVisit sorted such that the least risky points are at the end
+		// sort.Slice(toVisit, func(i, j int) bool {
+
+		// 	if toVisit[i].totalRisk == toVisit[j].totalRisk {
+		// 		return false
+		// 	}
+
+		// 	// unknown risk = sort at the beginning
+		// 	if toVisit[i].totalRisk == unknownRisk {
+		// 		return true
+		// 	}
+
+		// 	if toVisit[j].totalRisk == unknownRisk {
+		// 		return false
+		// 	}
+
+		// 	// known risk = the lower the number, the higher it is sorted
+		// 	return toVisit[i].totalRisk > toVisit[j].totalRisk
+		// })
+	}
+
+	// visited now contains all points, each with the lowest possible risk
+	// to get to them
+	for _, p := range visited {
+		if p.x == width-1 && p.y == height-1 {
+			return p.totalRisk
+		}
+	}
+
+	panic("Could not determine total risk")
+}
+
+func popPoint(slice []point) (point, []point) {
+	sliceLen := len(slice)
+	p := slice[sliceLen-1]
+	newSlice := make([]point, sliceLen-1)
+	copy(newSlice, slice)
+	return p, newSlice
+}
+
+func parseInput(r io.Reader) *[][]int {
+	var grid [][]int
+	var width int
+
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		line := strings.TrimSpace(s.Text())
+		if len(line) == 0 {
+			continue
+		}
+		row := make([]int, 0, width)
+		for _, r := range line {
+			num, err := strconv.ParseInt(string(r), 10, 8)
+			if err != nil {
+				panic(err)
+			}
+			row = append(row, int(num))
+		}
+		grid = append(grid, row)
+	}
+
+	return &grid
+}
---
+++ day15/puzzle01_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSolveDijkstra(t *testing.T) {
+	input := strings.NewReader(strings.TrimSpace(`
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+	`))
+
+	grid := parseInput(input)
+
+	lowestTotalRisk := solveDijkstra(grid)
+	if lowestTotalRisk != 40 {
+		t.Fatalf("Wrong answer -- expected %d, got %d", 40, lowestTotalRisk)
+	}
+
+}
+func TestInflateGrid(t *testing.T) {
+	input := strings.NewReader(strings.TrimSpace(`
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+	`))
+
+	grid := parseInput(input)
+
+	inflated := inflateGrid(grid, 5)
+
+	if (*inflated)[0][10] != 2 {
+		t.Fatalf("inflate failed")
+	}
+
+	if (*inflated)[49][49] != 9 {
+		t.Fatalf("inflate failed")
+	}
+
+}
