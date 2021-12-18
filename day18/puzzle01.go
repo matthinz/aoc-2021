87250a608e8bdbc57e1c64ba459fa2708f3b4b8f
Matt Hinz <matthinz@gmail.com>
2021-12-17 23:40:49 -0800

    Day 18, part 1 snailfish not working

--- day18/puzzle01.go
+++ day18/puzzle01.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
@@ -25,21 +26,270 @@ type snailfishNumber struct {
 func main() {
 
 	s := bufio.NewScanner(os.Stdin)
+
+	var num *snailfishNumber
+
 	for s.Scan() {
 		line := s.Text()
 		if len(line) == 0 {
 			continue
 		}
 
-		num, err := parseSnailfishNumber(line)
+		parsed, err := parseSnailfishNumber(line)
+
 		if err != nil {
 			panic(err)
 		}
 
-		fmt.Println(num.String())
-
+		if num == nil {
+			num = parsed
+		} else {
+			num = add(num, parsed)
+		}
 	}
 
+	fmt.Println(num.magnitude())
+
+}
+
+// clones this number. the clone will not have a parent set
+func (s *snailfishNumber) clone() *snailfishNumber {
+
+	if s == nil {
+		return nil
+	}
+
+	c := snailfishNumber{
+		kind:  s.kind,
+		value: s.value,
+		left:  s.left.clone(),
+		right: s.right.clone(),
+	}
+
+	if c.left != nil {
+		c.left.parent = &c
+	}
+	if c.right != nil {
+		c.right.parent = &c
+	}
+
+	return &c
+
+}
+
+// depth returns the # of ancestors a number has
+func (s *snailfishNumber) depth() int {
+	if s.parent == nil {
+		return 0
+	}
+	return s.parent.depth() + 1
+}
+
+func (s *snailfishNumber) explode() {
+
+	// To explode a pair, the pair's left value is added to the first regular
+	// number to the left of the exploding pair (if any), and the pair's right
+	// value is added to the first regular number to the right of the exploding
+	// pair (if any). Exploding pairs will always consist of two regular numbers.
+	// Then, the entire exploding pair is replaced with the regular number 0.
+
+	if s.kind != pairNumberKind {
+		return
+	}
+
+	haveTwoRegularNumbers := s.left.kind == regularNumberKind && s.right.kind == regularNumberKind
+
+	if !haveTwoRegularNumbers {
+		panic("should have two regular numbers")
+	}
+
+	// Add the left value to the first number to the left
+
+	var numberToTheLeft *snailfishNumber
+
+	for n := s; n != nil; n = n.parent {
+		isRight := n.parent != nil && n.parent.right == n
+		if !isRight {
+			continue
+		}
+		numberToTheLeft = n.parent.left
+		break
+	}
+
+	if numberToTheLeft != nil {
+		// find the rightmost literal inside numberToTheLeft
+		for numberToTheLeft.kind != regularNumberKind {
+			numberToTheLeft = numberToTheLeft.right
+		}
+
+		numberToTheLeft.value += s.left.value
+	}
+
+	// Add the right value to the first number to the right
+	var numberToTheRight *snailfishNumber
+	for n := s; n != nil; n = n.parent {
+		isLeft := n.parent != nil && n.parent.left == n
+		if !isLeft {
+			continue
+		}
+		numberToTheRight = n.parent.right
+		break
+	}
+
+	if numberToTheRight != nil {
+		// find the leftmost literal inside numberToTheRight
+		for numberToTheRight.kind != regularNumberKind {
+			numberToTheRight = numberToTheRight.left
+		}
+
+		numberToTheRight.value += s.right.value
+	}
+
+	// Reset the pair to the regular number "0"
+
+	s.kind = regularNumberKind
+	s.value = 0
+	s.left = nil
+	s.right = nil
+
+}
+
+func (s *snailfishNumber) findLeftmost(f func(*snailfishNumber) bool) *snailfishNumber {
+	if f(s) {
+		return s
+	}
+
+	if s.kind != pairNumberKind {
+		return nil
+	}
+
+	result := s.left.findLeftmost(f)
+	if result != nil {
+		return result
+	}
+
+	return s.right.findLeftmost(f)
+}
+
+func (s *snailfishNumber) findRightmost(f func(*snailfishNumber) bool) *snailfishNumber {
+	if f(s) {
+		return s
+	}
+	if s.kind != pairNumberKind {
+		return nil
+	}
+
+	result := s.right.findRightmost(f)
+	if result != nil {
+		return result
+	}
+
+	return s.left.findRightmost(f)
+
+}
+
+func (s *snailfishNumber) magnitude() int {
+	switch s.kind {
+	case regularNumberKind:
+		return s.value
+
+	case pairNumberKind:
+		return (3 * s.left.magnitude()) + (2 * s.right.magnitude())
+
+	default:
+		panic("invalid kind")
+	}
+}
+
+func (s *snailfishNumber) reduce() {
+
+	fmt.Printf("reducing: %s\n", s.String())
+
+	for {
+
+		leftmostNested := s.findLeftmost(func(n *snailfishNumber) bool {
+
+			fmt.Printf("visit: %s (%d)\n", n.String(), n.depth())
+
+			if n.kind != pairNumberKind {
+				return false
+			}
+
+			// it has to be *explodable*
+			if n.left.kind != regularNumberKind || n.right.kind != regularNumberKind {
+				return false
+			}
+
+			return n.depth() >= 4
+		})
+
+		if leftmostNested != nil {
+			value := leftmostNested.String()
+			before := s.String()
+			leftmostNested.explode()
+			fmt.Printf("reduce(): explode %s %s -> %s\n", value, before, s.String())
+			continue
+		}
+
+		leftmost10OrGreater := s.findLeftmost(func(n *snailfishNumber) bool {
+			return n.kind == regularNumberKind && n.value >= 10
+		})
+
+		if leftmost10OrGreater != nil {
+			value := leftmost10OrGreater.value
+			before := s.String()
+			leftmost10OrGreater.split()
+			fmt.Printf("reduce(): split %d %s -> %s\n", value, before, s.String())
+			continue
+		}
+
+		fmt.Printf("reduce(): result %s\n", s.String())
+		return
+	}
+}
+
+func (s *snailfishNumber) split() {
+	if s.kind != regularNumberKind {
+		return
+	}
+
+	left := snailfishNumber{
+		kind:   regularNumberKind,
+		value:  s.value / 2,
+		parent: s,
+	}
+
+	right := snailfishNumber{
+		kind:   regularNumberKind,
+		value:  int(math.Ceil(float64(s.value) / 2.0)),
+		parent: s,
+	}
+
+	s.kind = pairNumberKind
+	s.value = 0
+	s.left = &left
+	s.right = &right
+}
+
+func add(lhs *snailfishNumber, rhs *snailfishNumber) *snailfishNumber {
+
+	result := snailfishNumber{
+		kind:  pairNumberKind,
+		left:  lhs.clone(),
+		right: lhs.clone(),
+	}
+
+	if result.left != nil {
+		result.left.parent = &result
+	}
+
+	if result.right != nil {
+		result.right.parent = &result
+	}
+
+	result.reduce()
+
+	return &result
 }
 
 func parseSnailfishNumber(input string) (*snailfishNumber, error) {
---
+++ day18/puzzle01_test.go
@@ -0,0 +1,138 @@
+package main
+
+import "testing"
+
+func TestExplode(t *testing.T) {
+
+	input := "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"
+	number, err := parseSnailfishNumber(input)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	p := number.left // [3,[2,[1,[7,3]]]]
+	if p.String() != "[3,[2,[1,[7,3]]]]" {
+		t.Fatalf("number.left was wrong")
+	}
+
+	p = p.right // [2,[1,[7,3]]]
+	if p.String() != "[2,[1,[7,3]]]" {
+		t.Fatalf("number.right was wrong")
+	}
+
+	p = p.right // [1,[7,3]]
+	if p.String() != "[1,[7,3]]" {
+		t.Fatalf("number.right was wrong")
+	}
+
+	p = p.right // [7,3]
+	if p.String() != "[7,3]" {
+		t.Fatalf("number.right was wrong")
+	}
+
+	p.explode()
+
+	expected := "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+	actual := number.String()
+
+	if actual != expected {
+		t.Fatalf("explode() failed. expected %s, got %s", expected, actual)
+	}
+}
+
+func TestReduce(t *testing.T) {
+	input := "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"
+	number, err := parseSnailfishNumber(input)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	number.reduce()
+
+	expected := "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
+	actual := number.String()
+
+	if actual != expected {
+		t.Fatalf("reduce() failed. expected %s, got %s", expected, actual)
+	}
+}
+
+func TestSplit(t *testing.T) {
+
+	input := "[5,8]"
+	number, err := parseSnailfishNumber(input)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	n := number.left
+	n.split()
+	expected := "[2,3]"
+	actual := n.String()
+	if actual != expected {
+		t.Fatalf("split() failed. expected %s, got %s", expected, actual)
+	}
+
+	n = number.right
+	n.split()
+	expected = "[4,4]"
+	actual = n.String()
+	if actual != expected {
+		t.Fatalf("split() failed. expected %s, got %s", expected, actual)
+	}
+
+	expected = "[[2,3],[4,4]]"
+	actual = number.String()
+	if actual != expected {
+		t.Fatalf("split() failed. expected %s, got %s", expected, actual)
+	}
+}
+
+func TestSum(t *testing.T) {
+	input := []string{
+		"[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+		"[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+		"[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+		"[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+		"[7,[5,[[3,8],[1,4]]]]",
+		"[[2,[2,2]],[8,[8,1]]]",
+		"[2,9]",
+		"[1,[[[9,3],9],[[9,0],[0,7]]]]",
+		"[[[5,[7,4]],7],1]",
+		"[[[[4,2],2],6],[8,7]]",
+	}
+
+	expected := []string{
+		"[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+		"[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]",
+		"[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]",
+		"[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]",
+		"[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]",
+		"[[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]",
+		"[[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]",
+		"[[[[7,8],[6,7]],[[6,8],[0,8]]],[[[7,7],[5,0]],[[5,5],[5,6]]]]",
+		"[[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]",
+		"[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]",
+	}
+
+	var num *snailfishNumber
+
+	for i, s := range input {
+		n, err := parseSnailfishNumber(s)
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+
+		if num == nil {
+			num = n
+		} else {
+			num = add(num, n)
+		}
+
+		if num.String() != expected[i] {
+			t.Fatalf("Step %d failed: expected %s, got %s", i, expected[i], num.String())
+		}
+
+	}
+
+}
