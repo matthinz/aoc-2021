5026244ca404cbbd21e8445d5f8e9889d7bb26ee
Matt Hinz <matthinz@gmail.com>
2021-12-21 10:02:32 -0800

    Day 19 WIP

--- day19/beacons.go
+++ day19/beacons.go
@@ -28,9 +28,13 @@ type scannerRelation struct {
 
 	// becons in <b> that correspond to beacons in <a>
 	bBeacons []point
+
+	// vector describing how axes are rotated
+	rotationVector point
 }
 
 const MinBeaconsInCommon = 12
+const MaxScannerVisibility = 1000
 
 func main() {
 	l := log.New(os.Stderr, "", log.Default().Flags())
@@ -53,6 +57,32 @@ func Puzzle01(r io.Reader, l *log.Logger) string {
 	return strconv.Itoa(uniqueCount)
 }
 
+// returns a new point with each coordinate inverted
+func (p *point) inverse() point {
+	return point{
+		x: -1 * p.x,
+		y: -1 * p.y,
+		z: -1 * p.z,
+	}
+}
+
+func (p *point) multiply(vector point) point {
+	return point{
+		x: p.x * vector.x,
+		y: p.y * vector.y,
+		z: p.z * vector.z,
+	}
+}
+
+// returns a new point translated using the given vector
+func (p *point) translate(vector point) point {
+	return point{
+		x: p.x + vector.x,
+		y: p.y + vector.y,
+		z: p.z + vector.z,
+	}
+}
+
 func countBeaconsDetected(scanners []scanner) int {
 	count := 0
 	for _, scanner := range scanners {
@@ -62,90 +92,161 @@ func countBeaconsDetected(scanners []scanner) int {
 }
 
 func countUniqueBeaconsDetected(scanners []scanner) int {
-	// To count the total number of beacons:
-	// - go through each scanner
-	// - for each beacon on the scanner:
-	//   - if not visited, add 1
-	//   - find all
-	// - mark each
-
-	visited := make(map[string]bool)
-	count := 0
+	uniqueBeacons := make(map[point]bool)
 
 	for i, scanner := range scanners {
-		for _, beacon := range scanner.beacons {
+		for j := i + 1; j < len(scanners); j++ {
+			otherScanner := scanners[j]
+			rel := compareScanners(scanner, otherScanner)
 
-			key := fmt.Sprintf("%s.%v", scanner.name, beacon)
-			if visited[key] {
-				fmt.Printf("skip: %s\n", key)
+			if len(rel.aBeacons) < MinBeaconsInCommon {
 				continue
 			}
-			visited[key] = true
 
-			// find on all other scanners
-			for j := range scanners {
-				if i == j {
-					continue
-				}
-				rel := compareScanners(scanner, scanners[j])
+			fmt.Printf("%s -> %s (%d)\n", scanner.name, otherScanner.name, len(rel.aBeacons))
 
-				if len(rel.aBeacons) < MinBeaconsInCommon {
-					continue
-				}
-
-				for _, bBeacon := range rel.bBeacons {
-					key := fmt.Sprintf("%s.%v", scanners[j].name, bBeacon)
-					fmt.Printf("visited: %s\n", key)
-					visited[key] = true
-				}
-			}
-
-			count++
 		}
+
 	}
 
-	return count
+	return len(uniqueBeacons)
 }
 
 // compareScanners takes two scanners and returns a structure describing the
-// relation between each
+// relation between each, including which beacons are equivalent
 func compareScanners(a, b scanner) scannerRelation {
 
-	aDistances := buildBeaconDistanceMap(a.beacons)
-	bDistances := buildBeaconDistanceMap(b.beacons)
-
-	relation := scannerRelation{
-		a: a,
-		b: b,
-	}
-
-	for aBeacon, distancesInA := range aDistances {
-
-		var bestMatch *point
-		var bestScore int
-
-		// if a + b overlap on <aBeacon>, then there must be a single <bBeacon>
-		// that corresponds to it. This will be the beacon in <b> that shares the
-		// *most* sibling distances in common with *aBeacon*
-
-		for bBeacon, distancesInB := range bDistances {
-
-			score := similarity(distancesInA, distancesInB)
-
-			if score > bestScore {
-				bestMatch = &bBeacon
-				bestScore = score
+	rotationsToTry := make([]point, 0)
+	for _, xRotation := range []float64{-1, 1} {
+		for _, yRotation := range []float64{-1, 1} {
+			for _, zRotation := range []float64{-1, 1} {
+				rotationsToTry = append(rotationsToTry, point{xRotation, yRotation, zRotation})
 			}
 		}
+	}
 
-		if bestMatch != nil {
-			// we believe we have found a match for this beacon
-			relation.aBeacons = append(relation.aBeacons, aBeacon)
-			relation.bBeacons = append(relation.bBeacons, *bestMatch)
+	facingsToTry := []func(point) point{
+		func(p point) point { return point{p.x, p.y, p.z} },
+		func(p point) point { return point{p.x, p.z, p.y} },
+		func(p point) point { return point{p.y, p.x, p.z} },
+		func(p point) point { return point{p.y, p.z, p.x} },
+		func(p point) point { return point{p.z, p.x, p.y} },
+		func(p point) point { return point{p.z, p.y, p.x} },
+	}
+
+	nullRotation := point{1, 1, 1}
+
+	var bestResult *scannerRelation
+
+	for _, rotation := range rotationsToTry {
+
+		for _, facing := range facingsToTry {
+
+			relation := scannerRelation{
+				a:              a,
+				b:              b,
+				rotationVector: rotation,
+			}
+
+			for _, aBeacon := range a.beacons {
+				for _, bBeacon := range b.beacons {
+
+					// Here we assume aBeacon == bBeacon and come up with a new coordinate
+					// system with this point as its origin. We can then translate all
+					// beacons on <a> and <b> into this new coordinate system.
+
+					// deltaA can be added to points from <a> to convert them into our
+					// "universal" coordinate space
+					deltaA := aBeacon.inverse()
+
+					// deltaB can be added to points from <b> to convert them into our
+					// "universal" coordinate space
+					deltaB := bBeacon.inverse()
+
+					aBeaconsInUniversalSpace := rotateAndTranslateBeacons(a.beacons, nullRotation, deltaA)
+					aBeaconsInBSpace := rotateAndTranslateBeacons(aBeaconsInUniversalSpace, rotation, deltaB.inverse())
+					aBeaconsInBSpace = filterBeacons(aBeaconsInBSpace, beaconIsVisible)
+					aBeaconsInBSpace = mapBeacons(aBeaconsInBSpace, facing)
+
+					bBeaconsInUniversalSpace := rotateAndTranslateBeacons(b.beacons, nullRotation, deltaB)
+					bBeaconsInASpace := rotateAndTranslateBeacons(bBeaconsInUniversalSpace, rotation, deltaA.inverse())
+					bBeaconsInASpace = filterBeacons(bBeaconsInASpace, beaconIsVisible)
+					bBeaconsInASpace = mapBeacons(bBeaconsInASpace, facing)
+
+					// If all the a beacons translated into b space are present in b.beacons
+					// AND
+					// all the b beacons translated into a space are present in a.beacons
+					// then we have a rotation that worked!
+
+					if allBeaconsFound(aBeaconsInBSpace, b.beacons) &&
+						allBeaconsFound(bBeaconsInASpace, a.beacons) {
+						// we have found it
+						relation.aBeacons = append(relation.aBeacons, aBeacon)
+						relation.bBeacons = append(relation.bBeacons, bBeacon)
+					}
+				}
+			}
+
+			if bestResult == nil || len(relation.aBeacons) > len(bestResult.aBeacons) {
+				bestResult = &relation
+			}
 		}
 	}
 
-	return relation
+	return *bestResult
+}
+
+func mapBeacons(slice []point, f func(point) point) []point {
+	result := make([]point, len(slice))
+	for i := range slice {
+		result[i] = f(slice[i])
+	}
+	return result
+}
+
+func beaconIsVisible(beacon point) bool {
+	return math.Abs(beacon.x) <= MaxScannerVisibility && math.Abs(beacon.y) <= MaxScannerVisibility && math.Abs(beacon.z) <= MaxScannerVisibility
+}
+
+// returns true if every point in <slice> is found in <in>
+func allBeaconsFound(slice []point, in []point) bool {
+
+	for _, p := range slice {
+		found := false
+		for _, o := range in {
+			if p == o {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+
+	return true
+}
+
+func filterBeacons(slice []point, f func(point) bool) []point {
+	result := make([]point, 0, len(slice))
+	for _, beacon := range slice {
+		if f(beacon) {
+			result = append(result, beacon)
+		}
+	}
+	return result
+}
+
+// rotates each point in <slice> by multiplying it by <rotationVector>, then
+// adds <translationVector> to it.
+func rotateAndTranslateBeacons(slice []point, rotationVector point, translationVector point) []point {
+	result := make([]point, 0, len(slice))
+	for i := range slice {
+		rotated := slice[i].multiply(rotationVector)
+		translated := rotated.translate(translationVector)
+		result = append(result, translated)
+	}
+	return result
 }
 
 func buildBeaconDistanceMap(beacons []point) map[point][]float64 {
@@ -178,17 +279,24 @@ func distance(a, b point) float64 {
 	)
 }
 
-// similarity returns the number of values <a> and <b> have in common
-func similarity(a, b []float64) int {
-	var found int
-	for _, aValue := range a {
-		bIndex := sort.SearchFloat64s(b, aValue)
-		if bIndex < len(b) {
-			// value is found
-			found++
-		}
+// countDistancesInCommon returns the number of values <a> and <b> have in common
+func countDistancesInCommon(a, b []float64) int {
+	counts := make(map[float64]int)
+
+	for _, value := range a {
+		counts[value]++
 	}
-	return found
+
+	for _, value := range b {
+		counts[value]++
+	}
+
+	result := 0
+	for _, count := range counts {
+		result += count / 2
+	}
+
+	return result
 }
 
 func parseInput(r io.Reader) []scanner {
