52ecd5fa81389ead115e5f4377661989ad4e0c34
Matt Hinz <matthinz@gmail.com>
2021-12-28 08:53:09 -0800

    Day 20 WIP

---
+++ day20/enhance.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type image struct {
+	width  int
+	height int
+	pixels [][]bool
+}
+
+func main() {
+	l := log.New(os.Stderr, "", log.Default().Flags())
+	result := Puzzle01(os.Stdin, l)
+	fmt.Println(result)
+
+	os.Stdin.Seek(0, 0)
+	result = Puzzle02(os.Stdin, l)
+	fmt.Println(result)
+}
+
+func Puzzle01(r io.Reader, l *log.Logger) string {
+	img, algorithm := parseInput(r)
+
+	enhanced := enhance(&img, algorithm)
+
+	enhanced = enhance(enhanced, algorithm)
+
+	count := countLitPixels(enhanced)
+
+	return strconv.Itoa(count)
+}
+
+func Puzzle02(r io.Reader, l *log.Logger) string {
+	return ""
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// enhance!
+
+func enhance(img *image, algorithm []bool) *image {
+
+	result := image{
+		width:  img.width,
+		height: img.height,
+	}
+	result.pixels = make([][]bool, result.height)
+
+	for destY := 0; destY < result.height; destY++ {
+		result.pixels[destY] = make([]bool, result.width)
+
+		for destX := 0; destX < result.width; destX++ {
+
+			srcX := destX + ((img.width - result.width) / 2)
+			srcY := destY + ((img.height - result.height) / 2)
+
+			chunk := extractChunk(img, srcX, srcY)
+
+			index := chunkToIndex(chunk)
+
+			result.pixels[destY][destX] = algorithm[index]
+		}
+	}
+	return &result
+}
+
+func chunkToIndex(chunk [3][3]bool) int {
+	// chunk is read left-to-right, top-to-bottom as a 9 bit binary number
+	var result int
+
+	for y := 0; y < 3; y++ {
+		for x := 0; x < 3; x++ {
+			result <<= 1
+			if chunk[y][x] {
+				result = result | 1
+			}
+		}
+	}
+
+	return result
+}
+
+// extracts a 3x3 matrix from <img> centered on x,y
+func extractChunk(img *image, x int, y int) [3][3]bool {
+
+	chunk := [3][3]bool{}
+
+	for dY := -1; dY <= 1; dY++ {
+		for dX := -1; dX <= 1; dX++ {
+
+			chunkY := dY + 1
+			chunkX := dX + 1
+
+			actualY := y + dY
+			actualX := x + dX
+
+			var pixel bool
+
+			if actualY < 0 || actualY >= img.height {
+				pixel = false
+			} else if actualX < 0 || actualX >= img.width {
+				pixel = false
+			} else {
+				pixel = img.pixels[actualY][actualX]
+			}
+
+			chunk[chunkY][chunkX] = pixel
+
+		}
+	}
+
+	return chunk
+}
+
+func countLitPixels(img *image) int {
+	var count int
+	for y := 0; y < img.height; y++ {
+		for x := 0; x < img.width; x++ {
+			if img.pixels[y][x] {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// parseInput
+
+func parseInput(r io.Reader) (image, []bool) {
+
+	image := image{
+		pixels: make([][]bool, 0),
+	}
+	var algorithm []bool
+
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		l := strings.TrimSpace(s.Text())
+
+		if len(l) == 0 {
+			continue
+		}
+
+		if len(algorithm) == 0 {
+			algorithm = make([]bool, len(l))
+			for i, r := range l {
+				switch r {
+				case '#':
+					algorithm[i] = true
+				case '.':
+					algorithm[i] = false
+				default:
+					panic(fmt.Sprintf("Invalid char in algorithm: %s", string(r)))
+				}
+			}
+			continue
+		}
+
+		image.height++
+		image.width = len(l)
+
+		image.pixels = append(image.pixels, make([]bool, image.width))
+
+		for i, r := range l {
+			switch r {
+			case '#':
+				image.pixels[image.height-1][i] = true
+			case '.':
+				image.pixels[image.height-1][i] = false
+			default:
+				panic(fmt.Sprintf("Invalid char in image: %s", string(r)))
+			}
+		}
+	}
+
+	return image, algorithm
+}
---
+++ day20/enhance_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestChunkToIndex(t *testing.T) {
+	chunk := [3][3]bool{
+		{false, false, false},
+		{true, false, false},
+		{false, true, false},
+	}
+	expected := 34
+	actual := chunkToIndex(chunk)
+	if actual != expected {
+		t.Errorf("Expected %d (%b), got %d (%b)", expected, expected, actual, actual)
+	}
+}
+
+func TestEnhance(t *testing.T) {
+	input := strings.TrimSpace(`
+..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###
+	`)
+
+	img, algorithm := parseInput(strings.NewReader(input))
+
+	enhanced := enhance(&img, algorithm)
+
+	for y := 0; y < enhanced.height; y++ {
+		fmt.Println()
+		for x := 0; x < enhanced.width; x++ {
+			if enhanced.pixels[y][x] {
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+		}
+	}
+
+	expected := 24
+	actual := countLitPixels(enhanced)
+
+	if actual != expected {
+		t.Errorf("Wrong # of lit pixels. Expected %d, got %d", expected, actual)
+	}
+
+}
