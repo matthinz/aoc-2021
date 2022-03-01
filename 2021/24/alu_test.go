09ef80b834dd87c4c98ee864bb45e1e951fd967d
Matt Hinz <matthinz@gmail.com>
2022-02-28 19:43:51 -0800

    Day 24: Addl. test

--- 2021/24/alu_test.go
+++ 2021/24/alu_test.go
@@ -6,9 +6,28 @@ import (
 )
 
 func TestFindAllInputsInZ(t *testing.T) {
+	reg := parseInput(strings.NewReader(realInput))
+	expr := reg.z.Simplify(map[int]int{})
+	inputs := make(map[int]int)
+	expr.Accept(func(e Expression) {
+		if input, isInput := e.(*InputExpression); isInput {
+			inputs[input.index]++
+		}
+	})
+	if len(inputs) != 14 {
+		t.Errorf("Expected %d inputs, but got %d", 14, len(inputs))
+	}
+	for i := 0; i < 14; i++ {
+		_, ok := inputs[i]
+		if !ok {
+			t.Errorf("Input %d not found", i)
+		}
+	}
+}
 
-	t.Skip()
-
-	parseInput(strings.NewReader(realInput))
-
+func BenchmarkSimplify(b *testing.B) {
+	reg := parseInput(strings.NewReader(realInput))
+	for i := 0; i < b.N; i++ {
+		reg.z.Simplify(map[int]int{})
+	}
 }
